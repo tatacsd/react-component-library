4a9828b60ee8d9c3bbdbc2a9ecb2bbabd09e033f
thayscasado <thayscasado@hotmail.com>
2023-04-12 22:22:20 -0700

    ✨ added postcss settings on rollup
    this commit allow us to use css on the components

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
 import packageJson from './package.json' assert { type: 'json' };
 
 export default [
@@ -23,11 +24,13 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
+      postcss(),
     ],
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
